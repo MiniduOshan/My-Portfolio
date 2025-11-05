a23b56e13f9deb4cf4480f20ac6e2d86e5f23571
MiniduOshan <126508461+MiniduOshan@users.noreply.github.com>
2025-11-05 20:46:06 +0530

    fix base problem

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,11 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/My-Portfolio">
+    {/* The 'basename' prop has been removed from <BrowserRouter>.
+      This is because your site is now served from the root ( / )
+      of your custom domain (miniduoshan.me), not the /My-Portfolio/ sub-directory.
+    */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
